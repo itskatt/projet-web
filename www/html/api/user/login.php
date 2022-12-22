81c30a3d0876b85ba12cde2273822622562ac552
Itskatt <raph10@mail.com>
2022-12-22 15:35:05 +0100

    Mechanisme de login (verifications)

---
+++ www/html/api/user/login.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once "../_common/handler.php";
+
+class LoginHandler extends PublicHandler
+{
+    protected function handlePOST($data): void
+    {
+        $this->checkFields(
+            ["email", "password"],
+            $data
+        );
+
+        $conn = $this->getConnector();
+
+        $res = $conn->query(
+            <<<END
+            select email,
+                   password_ "password"
+            from client
+            where email = :email;
+            END,
+            [
+                "email" => $data["email"]
+            ]
+        )->fetch(PDO::FETCH_ASSOC);
+
+        $hash = $res["password"];
+
+        if (!password_verify($data["password"], $hash)) {
+            $this->sendError(
+                401,
+                "Email ou mot de passe incorect !"
+            );
+        }
+
+        // On est bon
+        // TODO : token et cie
+        $this->sendOK([]);
+    }
+}
+
+$handler = new LoginHandler();
+$handler->handle();
