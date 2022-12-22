6d042074c0c9ba0c4e3382c648be6e9cc34e026f
Itskatt <raph10@mail.com>
2022-12-22 19:41:02 +0100

    Ajouter la deconnexion

---
+++ www/html/api/user/logout.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once "../_common/handler.php";
+
+class LogoutHandler extends LoginRequiredHandler
+{
+    protected function handlePOST(?array $data): void
+    {
+        $token = $_COOKIE["token"];
+
+        // On supprime le cookie
+        setcookie("token", "", time() - 10);
+
+        // Et la/les session(s) dans la BDD
+        $conn = $this->getConnector();
+        $conn->query(
+            <<<END
+            delete from session
+            where client_email = (
+                select client_email from session
+                where token = :token
+                );
+            END,
+            ["token" => $token]
+        );
+
+        $this->sendOK([]);
+    }
+}
+
+$handler = new LogoutHandler();
+$handler->handle();
