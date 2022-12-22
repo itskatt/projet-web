ed58803e21c4b266fd623ca74b16d907816da959
Itskatt <raph10@mail.com>
2022-12-22 14:04:57 +0100

    Ajouter la route pour plusieurs articles

---
+++ www/html/api/public/articles.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once "../_common/handler.php";
+
+class MultipleArticleHandler extends PublicHandler
+{
+    protected function handleGET(): void
+    {
+        if (isset($_GET["page"])) {
+            // Par défault on revoie la première page
+            $page = 1;
+        } else {
+            $page = $_GET["page"];
+        }
+
+        $stop = $page * 20;
+        $start = $stop - 20;
+
+        $conn = $this->getConnector();
+
+        $res = $conn->query(
+            <<<END
+            select a.id "article_id",
+                a.name_ "article_name",
+                description_ "description",
+                rating,
+                year,
+                supplier_price,
+                image_ "image",
+                s.name_ "supplier_name",
+                quantity
+            from article a
+                    inner join supplier s on a.supplier_id = s.id
+                    inner join stock s2 on a.id = s2.article_id
+            order by a.id
+            limit $start, $stop;
+            END,
+        )->fetchAll(PDO::FETCH_ASSOC);
+
+        if (!$res) {
+            $this->sendError(
+                404,
+                "Cet page n'existe pas."
+            );
+        }
+
+        $this->sendOK([
+            "articles" => array_values($res)
+        ]);
+    }
+}
+
+$handler = new MultipleArticleHandler();
+$handler->handle();
