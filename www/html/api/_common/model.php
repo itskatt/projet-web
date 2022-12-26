e4b431e42f5bcf592a21980f48e49bc27875e7ec
Itskatt <raph10@mail.com>
2022-12-26 21:25:44 +0100

    Exposer les méthodes de commit/rollback dans le connecteur

--- www/html/api/_common/model.php
+++ www/html/api/_common/model.php
@@ -29,5 +29,29 @@ class DatabaseConnector
 
         return $result;
     }
+
+    /**
+     * Expose la méthode PDO::beginTransaction()
+     */
+    public function beginTransaction(): bool
+    {
+        return $this::$db->beginTransaction();
+    }
+
+    /**
+     * Expose la méthode PDO::commit()
+     */
+    public function commit(): bool
+    {
+        return $this::$db->commit();
+    }
+
+    /**
+     * Expose la méthode PDO::rollBack()
+     */
+    public function rollBack(): bool
+    {
+        return $this::$db->rollBack();
+    }
 }
 
