7e63f0a624676122173fc3504f1adc8cc2f8004a
Itskatt <raph10@mail.com>
2022-12-27 12:43:20 +0100

    Ajouter le prix total par articles au infos du panier

--- www/html/api/cart/current.php
+++ www/html/api/cart/current.php
@@ -20,7 +20,9 @@ class CurrentCartHandler extends LoginRequiredHandler
                     image_ "image",
                     s.name_ "supplier_name",
                     s2.quantity "stock_quantity",
-                    ca.quantity "cart_quantity"
+                    ca.quantity "cart_quantity",
+                    ca.quantity * supplier_price "price_no_tax",
+                    ca.quantity * supplier_price * 1.2 "price_tax"
             from article a
                     inner join supplier s on a.supplier_id = s.id
                     inner join stock s2 on a.id = s2.article_id
