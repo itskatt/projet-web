06591b0e24ae975586e5b59d2f3d97c356997291
Itskatt <raph10@mail.com>
2022-12-27 22:11:56 +0100

    Une commande réduit les stocks d'articles

--- www/html/api/cart/order.php
+++ www/html/api/cart/order.php
@@ -2,13 +2,13 @@
 
 require_once "../_common/handler.php";
 
-class OrderCartHandler extends LoginRequiredHandler 
+class OrderCartHandler extends LoginRequiredHandler
 {
     protected function handlePOST(?array $data): void
     {
-        $currentCardId =  $this->getCurrentCartId();
+        $currentCartId =  $this->getCurrentCartId();
 
-        if (!$currentCardId) {
+        if (!$currentCartId) {
             $this->sendError(
                 400,
                 "Il n'y a pas de panier actuelement commandable, veillez en créer un."
@@ -19,9 +19,45 @@ class OrderCartHandler extends LoginRequiredHandler
         $conn->query(
             <<<END
             insert into invoice (cart_id, created)
-            values ($currentCardId, now());
+            values ($currentCartId, now());
             END
         );
+
+        // Pour tout les articles dans le panier, faire baisser leur stock
+        $cartArticles = $conn->query(
+            <<<END
+            select article_id, quantity
+            from cart_article
+            where cart_id = $currentCartId;
+            END
+        )->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($cartArticles as $article) {
+            $stock = $conn->query(
+                <<<END
+                select quantity
+                    from stock
+                    where article_id = :aid;
+                END,
+                [
+                    "aid" => $article["article_id"]
+                ]
+            )->fetch(PDO::FETCH_NUM)[0];
+
+            $newQuantity = $stock - $article["quantity"];
+
+            $conn->query(
+                <<<END
+                update stock
+                set quantity = :quant
+                where article_id = :aid;
+                END,
+                [
+                    "quant" => $newQuantity,
+                    "aid" => $article["article_id"]
+                ]
+            );
+        }
 
         $invoiceId = $conn->query(
             <<<END
