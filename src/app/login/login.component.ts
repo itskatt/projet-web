74189f2bb111c67304654e7c052a438152cb6433
Itskatt <raph10@mail.com>
2023-01-02 00:06:53 +0100

    Factoriser le css des formulaires

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css', "../shared/forms.css"]
 })
 export class LoginComponent {
 
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css', "../shared/forms.css"]
 })
 export class RegisterComponent {
 
