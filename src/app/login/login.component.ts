6798fb1acbce9329d69b49802dcea2f1d6f62416
Itskatt <raph10@mail.com>
2023-01-02 20:01:04 +0100

    Début du code du formulaire de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css', "../shared/forms.css"]
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css', "../shared/forms.css"]
 })
 export class LoginComponent {
+    loginForm: FormGroup;
 
+    constructor() {
+        this.loginForm = new FormGroup({
+            email: new FormControl(""),
+            password: new FormControl(""),
+            remember: new FormControl(false)
+        });
+    }
+
+    submit(): void {
+        if (!this.loginForm.valid) {
+            return;
+        }
+
+        console.log(this.loginForm.value.email);
+        console.log(this.loginForm.value.password);
+        console.log(this.loginForm.value.remember);
+    }
 }
