a6a8b441db6c03fae826f25632d5f750094a3a19
Itskatt <raph10@mail.com>
2023-01-05 21:14:06 +0100

    Utiliser le composant cart pour tester l'utilisateur connecté

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+    selector: 'app-cart',
+    templateUrl: './cart.component.html',
+    styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+    email: string | null = null;
+
+    ngOnInit(): void {
+        this.email = localStorage.getItem("email")
+    }
 
 }
