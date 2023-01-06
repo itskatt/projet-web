2a087778fb31c01444690ca2b45797889357e163
Itskatt <raph10@mail.com>
2023-01-06 23:33:43 +0100

    Handler HTTP error in interceptor

--- src/app/service/http-interceptor.service.ts
+++ src/app/service/http-interceptor.service.ts
@@ -1,25 +1,39 @@
 import {
+    HttpErrorResponse,
     HttpEvent,
     HttpHandler,
     HttpInterceptor,
     HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class HttpInterceptorService implements HttpInterceptor {
-    constructor() {}
+    constructor(private router: Router) { }
     intercept(
         req: HttpRequest<any>,
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
-        return next.handle(
-            req.clone({
-                withCredentials: true,
-            })
-        );
+        return next.handle(req.clone({ withCredentials: true }))
+            .pipe(
+                catchError((error: HttpErrorResponse) => {
+                    switch (error.status) {
+                        case 401: // Unauthorised
+                            this.router.navigateByUrl("/login")
+                            break
+                    }
+
+                    // Not handled TODO global error handler
+                    console.group("Error in HTTP interceptor");
+                    console.error(error.status);
+                    console.error(error.error.message);
+                    console.groupEnd();
+                    return throwError(() => { error })
+                })
+            );
     }
 }
