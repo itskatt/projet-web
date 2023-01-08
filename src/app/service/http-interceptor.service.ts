0ef346f3a03d20545f409447c46d5e5bb90bd873
Itskatt <raph10@mail.com>
2023-01-08 14:40:17 +0100

    Effecer les données de dession dans le local storage quand 401

--- src/app/service/http-interceptor.service.ts
+++ src/app/service/http-interceptor.service.ts
@@ -8,20 +8,19 @@ import {
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, Observable, throwError } from 'rxjs';
+import { CurrentUserService } from './current-user.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class HttpInterceptorService implements HttpInterceptor {
-    constructor(private router: Router) {}
-    intercept(
-        req: HttpRequest<any>,
-        next: HttpHandler
-    ): Observable<HttpEvent<any>> {
+    constructor(private router: Router, private user: CurrentUserService) {}
+    intercept(req: HttpRequest<any>,next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req.clone({ withCredentials: true })).pipe(
             catchError((error: HttpErrorResponse) => {
                 switch (error.status) {
                     case 401: // Unauthorised
+                        this.user.localLogout();
                         this.router.navigateByUrl('/login');
                         break;
                 }
