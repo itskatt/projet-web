ef9ccbe51c5bc239fa6b754ab01183136e2a9ee5
Itskatt <raph10@mail.com>
2023-01-11 21:40:06 +0100

    Commencer très grossièrement le formulaire pour ajouter un nouvel article

--- src/app/admin-add-article/admin-add-article.component.ts
+++ src/app/admin-add-article/admin-add-article.component.ts
@@ -3,6 +3,8 @@ import { Component } from "@angular/core";
 @Component({
     selector: "app-admin-add-article",
     templateUrl: "./admin-add-article.component.html",
-    styleUrls: ["./admin-add-article.component.css"],
+    styleUrls: ["./admin-add-article.component.css", "../shared/forms.css"],
 })
-export class AdminAddArticleComponent {}
+export class AdminAddArticleComponent {
+    articleError: string = "";
+}
