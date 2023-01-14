146ecb3340175e44d187e14ea36ff1e3e8e7d891
Itskatt <raph10@mail.com>
2023-01-14 20:01:46 +0100

    Ajouter un apercu de l'image a envoyer

--- src/app/admin-add-article/admin-add-article.component.ts
+++ src/app/admin-add-article/admin-add-article.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
     selector: "app-admin-add-article",
@@ -11,4 +12,18 @@ import { Component } from "@angular/core";
 })
 export class AdminAddArticleComponent {
     articleError: string = "";
+
+    uploadImageUrl: any = "";
+
+    constructor(private sanitizer: DomSanitizer) {}
+
+    showImage(event: any): void {
+        this.uploadImageUrl = this.sanitizer.bypassSecurityTrustUrl(
+            URL.createObjectURL(event.target.files[0])
+        );
+    }
+
+    removeImage(): void {
+        this.uploadImageUrl = "";
+    }
 }
