07de7fec9f092c2a07f195e0683ffd76cb4a86d4
Itskatt <raph10@mail.com>
2023-01-16 14:40:26 +0100

    Ajouter l'upload des images coté serveur + redimention

--- php/api/admin/article.php
+++ php/api/admin/article.php
@@ -2,6 +2,19 @@
 
 require_once "../_common/handler.php";
 
+const IMAGE_HANDLERS = [
+    IMAGETYPE_JPEG => [
+        'load' => 'imagecreatefromjpeg',
+        'save' => 'imagejpeg',
+        'quality' => 95
+    ],
+    IMAGETYPE_PNG => [
+        'load' => 'imagecreatefrompng',
+        'save' => 'imagepng',
+        'quality' => 0
+    ]
+];
+
 /**
  * Opérations d'administration sur les articles.
  */
@@ -23,6 +36,115 @@ class AdminArticleHandler extends AdminRequiredHandler
         return (int) $object;
     }
 
+    /**
+     * Compresse une image pour qu'elle ai les dimentions souhaitée.
+     * Source : https://pqina.nl/blog/creating-thumbnails-with-php/
+     */
+    private function createThumbnail(string $src, string $dest, int $targetWidth, ?int $targetHeight = null)
+    {
+        $type = exif_imagetype($src);
+
+        if (!$type || !IMAGE_HANDLERS[$type]) {
+            return null;
+        }
+
+        $image = call_user_func(IMAGE_HANDLERS[$type]['load'], $src);
+
+        if (!$image) {
+            return null;
+        }
+
+        $width = imagesx($image);
+        $height = imagesy($image);
+
+        if ($targetHeight == null) {
+            $ratio = $width / $height;
+
+            if ($width > $height) {
+                $targetHeight = floor($targetWidth / $ratio);
+            } else {
+                $targetHeight = $targetWidth;
+                $targetWidth = floor($targetWidth * $ratio);
+            }
+        }
+
+        $thumbnail = imagecreatetruecolor($targetWidth, $targetHeight);
+
+        if ($type == IMAGETYPE_GIF || $type == IMAGETYPE_PNG) {
+            imagecolortransparent(
+                $thumbnail,
+                imagecolorallocate($thumbnail, 0, 0, 0)
+            );
+
+            if ($type == IMAGETYPE_PNG) {
+                imagealphablending($thumbnail, false);
+                imagesavealpha($thumbnail, true);
+            }
+        }
+
+        imagecopyresampled($thumbnail, $image, 0, 0, 0, 0, $targetWidth, $targetHeight, $width, $height);
+
+        return call_user_func(IMAGE_HANDLERS[$type]['save'], $thumbnail, $dest, IMAGE_HANDLERS[$type]['quality']);
+    }
+
+    /**
+     * Sauvegarde et traite l'image qui a été envoyé sur cette route et lui attribue un id.
+     * Renvoie null si aucune image n'est envoyée.
+     */
+    private function processImage(): ?string
+    {
+        if (!isset($_FILES["upl_img"])) {
+            // Aucune image n'a été envoyé
+            return null;
+        }
+
+        if ($_FILES["upl_img"]["error"] !== UPLOAD_ERR_OK) {
+            // Erreur lors de l'envoie de l'image
+            return null;
+        }
+
+        $fileName = $_FILES["upl_img"]['name'];
+        $fileSize = $_FILES["upl_img"]['size'];
+        $fileTmpName  = $_FILES["upl_img"]['tmp_name'];
+        $fileType = $_FILES["upl_img"]['type'];
+
+        $boom = explode('.', $fileName);
+        $fileExtension = strtolower(end($boom));
+
+        if (
+            !in_array($fileExtension, ["png", "jpeg", "jpg"]) or
+            !in_array($fileType, ['image/jpeg', 'image/jpg', 'image/png']) or
+            !in_array(getimagesize($fileTmpName)["mime"], ['image/jpeg', 'image/jpg', 'image/png'])
+        ) {
+            // On autorise que les jpeg et les png
+            $this->sendError(
+                400,
+                "Format d'image invalide"
+            );
+            return null;
+        }
+
+        $destDir = realpath(__DIR__ . "/../../uploads");
+        $imageId = uniqid() . "." . $fileExtension;
+
+        $tempDest = $destDir . "/TMP-" . $imageId;
+        if (!move_uploaded_file($fileTmpName, $tempDest)) {
+            $this->sendError(
+                500,
+                "Impossible de deplacer l'image, verifiez les permissions du dossier hôte."
+            );
+            return null;
+        }
+
+        // Ensuite on redimentionne l'image dans le format souhaité
+        $this->createThumbnail($tempDest, $destDir . "/" . $imageId, 400, 300);
+
+        // On supprime l'image non-redimentionée
+        unlink($tempDest);
+
+        return $imageId;
+    }
+
     protected function handlePOST(?array $data): void
     {
         $data = $_POST; // pour pouvoir accepter l'image, on remplace le json par du multipart/form-data
@@ -39,7 +161,6 @@ class AdminArticleHandler extends AdminRequiredHandler
         $supplierName = htmlspecialchars($data["supplier_name"]);
         $description = htmlspecialchars($data["description"]);
 
-        
         $rating = $this->validateInt($data, "rating");
 
         if ($rating > 5 or $rating < 0) {
@@ -61,6 +182,9 @@ class AdminArticleHandler extends AdminRequiredHandler
             );
         }
         $supplierPrice = (float) $supplierPrice;
+
+        echo $this->processImage();
+        die();
 
         $conn = $this->getConnector();
         // Premièrement, on s'occupe du fournisseur
